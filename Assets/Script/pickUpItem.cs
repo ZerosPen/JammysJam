e1a0f8dce589bbcbabdb763eeea95e7883de9fb2
ZerosPen <andhikaatmaja51@gmail.com>
2025-01-10 19:02:49 +0700

    0.2
    update on pick up fruit is 50% done
    minor in UI update and need animation

--- Assets/Script/pickUpItem.cs
+++ Assets/Script/pickUpItem.cs
@@ -1,15 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class pickUpItem : MonoBehaviour
 {
-    int item;
-    public void pickUpItems()
+    int apples = 0;
+    int pineApples = 0;
+    int peanuts = 0;
+    int counter = 0;
+
+    public GameObject fruitApple;
+    public GameObject fruitPineApple;
+    public GameObject fruitPeanut;
+
+    [SerializeField] private Text scoreText;
+
+   private void Start()
+{
+    if (scoreText == null)
     {
-        item++;
-        Debug.Log("item apple " + item );
+            scoreText = GetComponent<Text>();
+            if (scoreText == null)
+        {
+            Debug.LogError("Score Text not found in the scene!");
+        }
+    }
+    UpdateScoreText();
+}
+
+    public void PickUpItemApples()
+    {
+        apples++;
+        Debug.Log("Apples been add " + apples);
+        Instantiate(fruitApple, transform.position, Quaternion.identity);
+        counter++;
+        Debug.Log("total fruit = " + counter);
+        UpdateScoreText();
+    }
+
+    public void PickUpItemPineApples()
+    {
+        pineApples++;
+        Debug.Log("pineApples been add " + pineApples);
+        Instantiate(fruitPineApple, transform.position, Quaternion.identity);
+        counter++;
+        Debug.Log("total fruit = " + counter);
+        UpdateScoreText();
+    }
+    public void PickUpItempeanuts()
+    {
+        peanuts++;
+        Debug.Log("peanuts been add " + peanuts);
+        Instantiate(fruitPeanut, transform.position, Quaternion.identity);
+        counter++;
+        Debug.Log("total fruit = " + counter);
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        if (scoreText == null)
+        {
+            Debug.LogError("Score Text is not assigned!");
+            return;
+        }
+
+        scoreText.text = counter.ToString();
+    }
+
+
+    public void UnlockNext()
+    {
+        if (counter > 0)
+        {
+            
+        }
     }
 }
